02fce90b7cecab70b38adf293d96da23801e386e
Gareth Slinn <gslinn@gmail.com>
2021-06-30 20:59:40 +0100

    apply remove falsy functions

--- js/mergeArrays.js
+++ js/mergeArrays.js
@@ -1,30 +1,5 @@
 // mergeArrays(people,people2);
 
-const people = [
-    { firstName: 'John', secondName: 'Smith', age: '61',  },
-    { firstName: 'John', secondName: 'West', age: '55',  },
-    { firstName: 'Brenda', secondName: 'Holt', age: '60',  },
-    { firstName: 'Sally', secondName: 'Brampton', age: '33',  },
-    { firstName: 'June', secondName: 'Chester', age: '47',  },
-    { firstName: 'Jack', secondName: 'Carter', age: '24',  },
-    { firstName: 'Jack', secondName: 'Foster', age: '58',  },
-    { firstName: 'Jack', secondName: 'Foster', age: '58',  },
-    { firstName: 'Jack', secondName: 'Foster', age: '58',  },
-];
-
-const people2 = [
-    { firstName: 'John', secondName: 'Smith', age: '61',  },
-    { firstName: 'John', secondName: 'West', age: '55',  },
-    { firstName: 'Brenda', secondName: 'Holt', age: '60',  },
-    { firstName: 'Brenda', secondName: 'Holt', age: '60',  },
-    { firstName: 'Brenda', secondName: 'Holt', age: '60',  },
-    { firstName: 'Brenda', secondName: 'Holt', age: '60',  },
-    { firstName: 'Sally', secondName: 'Brampton', age: '33',  },
-    { firstName: 'June', secondName: 'Chester', age: '47',  },
-    { firstName: 'Jack', secondName: 'Carter', age: '24',  },
-    { firstName: 'Jack', secondName: 'Foster', age: '58',  }
-];
-
 export function mergeArrays(arr1, arr2) {
    const mergedArr = [...new Set([...arr1, ...arr2])];
 }

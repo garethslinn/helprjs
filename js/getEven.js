d7aaa4a57e8a213834f59f280b999d7120c5bd06
Gareth Slinn <gslinn@gmail.com>
2021-06-30 20:53:50 +0100

    apply remove falsy functions

--- js/getEven.js
+++ js/getEven.js
@@ -1,14 +1,9 @@
 // getEven(people,'age');
 // get even numbers only
 
-export function getEven(arr, key) {
-    const result = arr.map(function (item) {
-        if (parseInt(item[key]) % 2 == 0) {
-            return item
-        }
-    });
-    return result.filter(x => x !== undefined);
-};
+function getOdd(arr, key) {
+    return arr.filter(item => item[key] % 2 == 0);
+}
 
 
 
--- js/getOdd.js
+++ js/getOdd.js
@@ -1,14 +1,9 @@
 // getOdd(people,'age');
 // get odd numbers only
 
-export function getOdd(arr, key) {
-    const result = arr.map(function (item) {
-        if (parseInt(item[key]) % 2 == 1) {
-            return item
-        }
-    });
-    return result.filter(x => x !== undefined);
-};
+function getOdd(arr, key) {
+    return arr.filter(item => item[key] % 2 == 1);
+}
 
 
 

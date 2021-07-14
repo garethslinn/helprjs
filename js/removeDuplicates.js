1134053b3986be3597c72b0997488dac8a7054fa
Gareth Slinn <gslinn@gmail.com>
2021-07-14 09:23:39 +0100

    Update version number

--- js/removeDuplicates.js
+++ js/removeDuplicates.js
@@ -1,6 +1,6 @@
 // const newArray = removeDuplicates(people, 'firstName');
 // console.log("remaing objects: " + JSON.stringify(newArray));
 
-function removeDuplicates(arr, key) {
+export function removeDuplicates(arr, key) {
     return [...new Map(arr.map(item => [item[key], item])).values()]
 }

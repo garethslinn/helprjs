2145e6b9369f07eae820f1086086de6f5b3706d1
Gareth Slinn <gslinn@gmail.com>
2022-02-12 22:04:38 +0000

    fix the compiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 const {src, dest, series, watch} = require('gulp');
+const concat = require('gulp-concat');
 const ts = require('gulp-typescript');
 const uglify = require('gulp-uglify');
 
 function compileTs() {
-    return src('./js/**/*.ts')
+    return src('./ts/**/*.ts')
     .pipe(ts())
     .pipe(uglify())
-    .pipe(dest('./dist'));
+    .pipe(concat('index.js', {newLine: ';'}))
+    .pipe(dest('./dist/'));
 }
 
 exports.default = function () {
-    watch( ['**/*.ts'], series(compileTs) );
+    watch( ['**/*.ts'], series(compileTs) ),
+    done => {
+        done();
+    }
 };
+

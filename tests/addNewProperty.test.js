cadd71560b04f0f28aa7c8e918f2d35e09d527f8
Gareth Slinn <gslinn@gmail.com>
2022-02-22 22:06:20 +0000

    fix typo in addNewProperty test

--- tests/addNewProperty.test.js
+++ tests/addNewProperty.test.js
@@ -2,10 +2,10 @@ import { addNewProperty } from '../dist/index.js';
 import { people } from '../tests/peopleObject.js'
 
 describe('addNewProperty', () => {
-    test('object does not contain registered', () => {
+    test('original object does not contain registered', () => {
         expect(people[0].registered).toBe(undefined);
     });
-    test('object contains registered', () => {
+    test('transformed object contains registered', () => {
         const result = addNewProperty(people, 'registered', true)
         expect(result[0].registered).toBe(true);
     });

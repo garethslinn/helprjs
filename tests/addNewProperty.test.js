d1dc9afdc02f80232889fa4530f435588f55e65b
Gareth Slinn <gslinn@gmail.com>
2022-02-22 22:02:44 +0000

    fix typo in addNewProperty test

--- tests/addNewProperty.test.js
+++ tests/addNewProperty.test.js
@@ -5,7 +5,7 @@ describe('addNewProperty', () => {
     test('object does not contain registered', () => {
         expect(people[0].registered).toBe(undefined);
     });
-    test('aobject contains registered', () => {
+    test('object contains registered', () => {
         const result = addNewProperty(people, 'registered', true)
         expect(result[0].registered).toBe(true);
     });

228857d060b20191f372a5533adb055d8df3b8b1
Gareth Slinn <gslinn@gmail.com>
2022-02-22 22:00:34 +0000

    improve addNewProperty test

--- tests/addNewProperty.test.js
+++ tests/addNewProperty.test.js
@@ -2,7 +2,10 @@ import { addNewProperty } from '../dist/index.js';
 import { people } from '../tests/peopleObject.js'
 
 describe('addNewProperty', () => {
-    test('added extra key / value', () => {
+    test('object does not contain registered', () => {
+        expect(people[0].registered).toBe(undefined);
+    });
+    test('aobject contains registered', () => {
         const result = addNewProperty(people, 'registered', true)
         expect(result[0].registered).toBe(true);
     });

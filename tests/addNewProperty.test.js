e0ba1970f46b2a586d063512f4b6452ba55ed3e7
Gareth Slinn <gslinn@gmail.com>
2022-02-22 21:20:20 +0000

    add tests for countKeys and countObjects

--- tests/addNewProperty.test.js
+++ tests/addNewProperty.test.js
@@ -2,6 +2,6 @@ import { addNewProperty } from '../dist/index.js';
 import { people } from '../tests/peopleObject.js'
 
 test('addNewProperty: added extra key / value', () => {
-    const result = addNewProperty(people, 'registered', 'yes')
-    expect(result[0].registered).toBe('yes');
+    const result = addNewProperty(people, 'registered', true)
+    expect(result[0].registered).toBe(true);
 });

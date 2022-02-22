b156e45e3f291683074772ea2f4c41b020f521ac
Gareth Slinn <gslinn@gmail.com>
2022-02-22 21:54:53 +0000

    fix addNewProperty test

--- tests/addNewProperty.test.js
+++ tests/addNewProperty.test.js
@@ -1,7 +1,9 @@
 import { addNewProperty } from '../dist/index.js';
 import { people } from '../tests/peopleObject.js'
 
-test('addNewProperty: added extra key / value', () => {
-    const result = addNewProperty(people, 'registered', true)
-    expect(result[0].registered).toBe(true);
+describe('addNewProperty', () => {
+    test('added extra key / value', () => {
+        const result = addNewProperty(people, 'registered', true)
+        expect(result[0].registered).toBe(true);
+    });
 });
